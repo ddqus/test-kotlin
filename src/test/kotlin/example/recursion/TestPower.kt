1d36f64a922493f444d743ae5a53106f697ecdfc
ddqus <ddqus@users.noreply.github.com>
2022-01-31 10:40:01 +0900

    Refactor power using FP

--- src/test/kotlin/example/recursion/TestPower.kt
+++ src/test/kotlin/example/recursion/TestPower.kt
@@ -4,17 +4,17 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 class TestPower {
-    private fun power(x: Double, n: Int): Double {
-        val result: Double = when {
-            n <= 1 -> x
-            else -> x * power(x, n - 1)
+    private tailrec fun power(x: Double, n: Int, carry: Double = 1.0): Double {
+        return when (n) {
+            0 -> carry
+            else -> power(x, n - 1, x * carry)
         }
-        return result
     }
 
     @Test
     internal fun `test power`() {
         assertThat(power(2.0, 3)).isEqualTo(8.0)
         assertThat(power(2.0, 31) - 1).isEqualTo(Int.MAX_VALUE.toDouble())
+        assertThat(power(3.0, 3)).isEqualTo(27.0)
     }
 }
