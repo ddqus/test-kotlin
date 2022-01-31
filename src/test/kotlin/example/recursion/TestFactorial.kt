60db3560e05c6c3712e6bf48ee3d37515888b696
ddqus <ddqus@users.noreply.github.com>
2022-01-31 09:34:40 +0900

    Refactor factorial using counter

--- src/test/kotlin/example/recursion/TestFactorial.kt
+++ src/test/kotlin/example/recursion/TestFactorial.kt
@@ -2,13 +2,21 @@ package example.recursion
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
 import java.math.BigDecimal
 
 class TestFactorial {
+    var counter = 0
+
     private fun factorial(n: BigDecimal): BigDecimal {
         val result = when {
             n <= BigDecimal.ONE -> BigDecimal.ONE
-            else -> n * factorial(n - BigDecimal.ONE)
+            else -> {
+                counter++
+                n * factorial(n - BigDecimal.ONE)
+            }
         }
         return result
     }
@@ -19,5 +27,23 @@ class TestFactorial {
             .isEqualTo(BigDecimal("24"))
         assertThat(factorial(BigDecimal("50")))
             .isEqualTo(BigDecimal("30414093201713378043612608166064768844377641568960512000000000000"))
+    }
+
+    @ParameterizedTest
+    @MethodSource("linearRecursionSource")
+    internal fun counter(n: Int, count: Int) {
+        assertThat(counter).isEqualTo(0)
+        factorial(BigDecimal(n))
+        assertThat(counter).isEqualTo(count)
+    }
+
+    companion object {
+        @JvmStatic
+        fun linearRecursionSource() = listOf(
+            // factorial 은 n의 1승이니 선형적으로 증가한다?
+            Arguments.of(10, 9),
+            Arguments.of(11, 10),
+            Arguments.of(50, 49),
+        )
     }
 }
