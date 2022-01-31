088e66ae663f6fdc9def5c2c8c061bcc22d2022a
ddqus <ddqus@users.noreply.github.com>
2022-01-31 09:27:15 +0900

    Refactor fibo using parameterized test

--- src/test/kotlin/example/recursion/TestFibo.kt
+++ src/test/kotlin/example/recursion/TestFibo.kt
@@ -2,16 +2,21 @@ package example.recursion
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
 
 class TestFibo {
     var counter = 0
 
     private fun fibo(n: Int): Int {
-        counter++
         val result = when {
             n == 0 -> 0
             n == 1 -> 1
-            else -> fibo(n - 1) + fibo(n - 2)
+            else -> {
+                counter++
+                fibo(n - 1) + fibo(n - 2)
+            }
         }
         return result
     }
@@ -25,12 +30,23 @@ class TestFibo {
         assertThat(fibo(4)).isEqualTo(3)
     }
 
-    @Test
-    internal fun `count recursion`() {
+    @ParameterizedTest
+    @MethodSource("geometricSeriesRecursionSource")
+    internal fun `count recursion`(n: Int, expectedCount: Int) {
+        // TODO 왜 counter가 초기화되지? 누적되야 하는데?
         assertThat(counter).isEqualTo(0)
+        fibo(n)
+        assertThat(counter).isEqualTo(expectedCount)
+    }
 
-        fibo(20)
-        // 기하급수적으로 recursion 발생
-        assertThat(counter).isEqualTo(21891)
+    companion object {
+        @JvmStatic
+        fun geometricSeriesRecursionSource() = listOf(
+            // 기하급수적으로 recursion 발생
+            Arguments.of(18, 4180),
+            Arguments.of(19, 6764),
+            Arguments.of(20, 10945),
+            Arguments.of(21, 17710),
+        )
     }
 }
