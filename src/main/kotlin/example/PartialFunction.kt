2b9a4b875a14cabfe37d7185e5d6f5efcf8c6acb
ddqus <ddqus@users.noreply.github.com>
2022-02-01 12:07:35 +0900

    Test partial function with invokeElse, orElse

--- src/main/kotlin/example/PartialFunction.kt
+++ src/main/kotlin/example/PartialFunction.kt
@@ -1,6 +1,6 @@
 package example
 
-class PartialFunction<in P, out R>(
+class PartialFunction<P, R>(
     private val condition: (P) -> Boolean,
     private val f: (P) -> R
 ) : (P) -> R {
@@ -14,4 +14,23 @@ class PartialFunction<in P, out R>(
     }
 
     fun isDefinedAt(p: P): Boolean = condition(p)
+
+    fun invokeOrElse(p: P, default: R): R = when {
+        isDefinedAt(p) -> invoke(p)
+        else -> default
+    }
+
+    fun orElse(that: PartialFunction<P, R>): PartialFunction<P, R> {
+        val func = PartialFunction(
+            { value: P -> this.isDefinedAt(value) || that.isDefinedAt(value) },
+            { value: P ->
+                when {
+                    this.isDefinedAt(value) -> this(value)
+                    that.isDefinedAt(value) -> that(value)
+                    else -> throw IllegalArgumentException("$value isn't defined")
+                }
+            }
+        )
+        return func
+    }
 }
--- src/test/kotlin/example/PartialFunctionTest.kt
+++ src/test/kotlin/example/PartialFunctionTest.kt
@@ -56,4 +56,49 @@ internal class PartialFunctionTest {
         assertThat(isEven.isDefinedAt(3)).isFalse
         assertThat(isEven(4)).isTrue
     }
+
+    @Test
+    internal fun `test invokeElse`() {
+        val condition: (Int) -> Boolean = { it % 2 == 0 }
+        val body: (Int) -> Boolean = { it % 2 == 0 }
+        val isEven = body.toPartialFunction(condition)
+
+        assertThat(isEven.invokeOrElse(1, true)).isTrue
+        assertThat(isEven.invokeOrElse(1, false)).isFalse
+        assertThat(isEven.invokeOrElse(2, true)).isTrue
+        assertThat(isEven.invokeOrElse(2, false)).isTrue
+    }
+
+    @Test
+    internal fun `test orElse`() {
+        val conditionA: (Int) -> Boolean = { it % 2 == 0 }
+        val bodyA: (Int) -> String = { "$it is 2의 배수" }
+        val isTwo = bodyA.toPartialFunction(conditionA)
+
+        val conditionB: (Int) -> Boolean = { it % 3 == 0 }
+        val bodyB: (Int) -> String = { "$it is 3의 배수" }
+        val isThree = bodyB.toPartialFunction(conditionB)
+
+        assertThatThrownBy { isTwo(1) }
+            .hasMessage("1 isn't supported")
+        assertThat(isTwo(2)).isEqualTo("2 is 2의 배수")
+        assertThatThrownBy { isTwo(3) }
+            .hasMessage("3 isn't supported")
+
+        assertThatThrownBy { isThree(1) }
+            .hasMessage("1 isn't supported")
+        assertThatThrownBy { isThree(2) }
+            .hasMessage("2 isn't supported")
+        assertThat(isThree(3)).isEqualTo("3 is 3의 배수")
+
+        assertThat(isTwo(6)).isEqualTo("6 is 2의 배수")
+        assertThat(isThree(6)).isEqualTo("6 is 3의 배수")
+
+        val isTwoOrThree = isTwo.orElse(isThree)
+        assertThatThrownBy { isTwoOrThree(1) }
+            .hasMessage("1 isn't supported")
+        assertThat(isTwoOrThree(2)).isEqualTo("2 is 2의 배수")
+        assertThat(isTwoOrThree(3)).isEqualTo("3 is 3의 배수")
+        assertThat(isTwoOrThree(6)).isEqualTo("6 is 2의 배수")
+    }
 }
